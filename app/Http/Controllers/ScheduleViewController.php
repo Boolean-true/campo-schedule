c930f6070dc364a34db80bec279e759b54bf1739
404Simon <s.wi@mail.de>
2025-10-14 11:36:25 +0200

    feat: merge in livewire starterkit

--- app/Http/Controllers/ScheduleViewController.php
+++ app/Http/Controllers/ScheduleViewController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ScheduleViewController extends Controller
+class ScheduleViewController
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
     {
         return view('schedule');
