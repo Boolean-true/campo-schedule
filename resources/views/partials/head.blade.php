8e15b451da6d867ef51f113fd3f823d610cbe0d4
404Simon <s.wi@mail.de>
2025-10-15 22:35:25 +0200

    feat: pwa

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,12 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+<!-- PWA Capabilities -->
+<link rel="manifest" href="/manifest.json" />
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="apple-touch-icon" href="/favicon.png">
+<link rel="icon" sizes="192x192" href="/favicon-192x192.png">
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
